6c635544bdc286dd1e74662f11dbf2ec2b8bb367
Ali.O <ali.ouroui@outlook.fr>
2026-03-17 11:06:53 +0100

    Added ScrollProgress and ScrollTopButton to Project page; updated styles

--- portfolio/src/layout/MainLayout.jsx
+++ portfolio/src/layout/MainLayout.jsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollProgress from "../components/ScrollProgress";
+import ScrollTopButton from "../components/ScrollTopButton";
 
 function MainLayout() {
   return (
     <>
       <Navbar />
-
+      <ScrollProgress />
       <main>
         <Outlet />
       </main>
-
+      <ScrollTopButton />
       <Footer />
     </>
   );
